0f7fe29c43e6cfa2480c72064cc9bb3c8ec40302
“squirrelgirl58” <kdillon2@nd.edu>
2016-12-14 17:55:34 -0500

    add admin to students

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214223240) do
+ActiveRecord::Schema.define(version: 20161214225507) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "c_name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20161214223240) do
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "admin"
   end
 
 end
